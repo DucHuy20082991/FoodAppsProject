268c88f7e49328cd18c31a3b32468a3bc8709ffa
Huy <svnguyenvuduchuy20082991@gmail.com>
2023-11-26 22:35:35 +0700

    info foods

--- components/Detail.js
+++ components/Detail.js
@@ -46,6 +46,29 @@ const Detail = ({ route }) => {
       <View style={styles.priceWrapper}>
         <Text style={styles.priceText}>${item.price}</Text>
       </View>
+
+
+      <View style={styles.infoWrapper}>
+        <View style={styles.infoLeftWrapper}>
+          <View style={styles.infoItemWrapper}>
+            <Text style={styles.infoItemTitle}>Size</Text>
+            <Text style={styles.infoItemText}>
+              {item.sizeName} {item.sizeNumber}"
+            </Text>
+          </View>
+          <View style={styles.infoItemWrapper}>
+            <Text style={styles.infoItemTitle}>Crust</Text>
+            <Text style={styles.infoItemText}>{item.crust}</Text>
+          </View>
+          <View style={styles.infoItemWrapper}>
+            <Text style={styles.infoItemTitle}>Delivery in</Text>
+            <Text style={styles.infoItemText}>{item.deliveryTime} min</Text>
+          </View>
+        </View>
+        <View>
+          <Image source={item.image} style={styles.itemImage} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -92,6 +115,27 @@ const styles = StyleSheet.create({
     color: colors.price,
     fontFamily: 'Montserrat-Bold',
     fontSize: 32,
+  },infoWrapper: {
+    marginTop: 60,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  infoLeftWrapper: {
+    paddingLeft: 20,
+  },
+  infoItemWrapper: {
+    marginBottom: 20,
+  },
+  infoItemTitle: {
+    fontFamily: 'Montserrat-Medium',
+    fontSize: 14,
+    color: colors.textLight,
+  },
+  infoItemText: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 18,
+    color: colors.textDark,
   },
 });
 
