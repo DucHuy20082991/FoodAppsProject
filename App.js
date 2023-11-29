851827113d029aec4bc3d3b728e4516c544d9193
unknown <thaitrongtindev@gmail.com>
2023-11-29 10:50:38 +0700

    inital seafood ui

--- App.js
+++ App.js
@@ -3,9 +3,7 @@ import * as React from 'react';
 import {Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from './components/Home';
 import Detail from './components/Detail';
 import SplashScreen from './components/SplashScreen';
@@ -15,68 +13,94 @@ import Register from './components/Register';
 import Login from './components/Login';
 import Forgotpassword from './components/Forgotpassword';
 
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Stack = createStackNavigator();
 
 function StackNavigator() {
-  return ( 
-    <Stack.Navigator initialRouteName="SplashScreen">
-        <Stack.Screen
-            name="Forgotpassword"
-            component={Forgotpassword}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{
-              headerShown: false,
-            }}
-          />
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Forgotpassword"
+        component={Forgotpassword}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
 
-          <Stack.Screen
-            name="Register"
-            component={Register}
-            options={{
-              headerShown: false,
-            }}
-          />
+      <Stack.Screen
+        name="Register"
+        component={Register}
+        options={{
+          headerShown: false,
+        }}
+      />
 
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="SplashScreen"
-            component={SplashScreen}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="Detail"
-            component={Detail}
-            options={{
-              headerShown: false,
-            }}
-          />
-   
-        </Stack.Navigator>
-  )
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="SplashScreen"
+        component={SplashScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Detail"
+        component={Detail}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
 }
 
+const Tab = createBottomTabNavigator();
 
+function BottomTabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={StackNavigator}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Details"
+        component={Detail}
+        options={{
+          tabBarLabel: 'Details',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="details" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        
+        <BottomTabNavigator></BottomTabNavigator>
       </NavigationContainer>
     </Provider>
   );
---
+++ components/Seafood.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Seafood = () => {
+  return (
+    <View>
+      <Text>Seafood</Text>
+    </View>
+  )
+}
+
+export default Seafood
