4dd54e30cae8daf1f56738ba3614bda37f59313a
unknown <thaitrongtindev@gmail.com>
2023-11-29 14:46:49 +0700

    code header and title seafood ui

--- components/Seafood.js
+++ components/Seafood.js
@@ -160,7 +160,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingHorizontal: 20,
-    paddingTop: 20,
+    paddingTop: 21,
     alignItems: 'center',
   },
   profileImage: {
